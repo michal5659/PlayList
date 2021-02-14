8faed525f497f370c7837808c33237d5d6bbca49
michal5659 <mkaner220@gmail.com>
2021-02-14 18:57:37 +0200

    gbg

--- client/src/app/shared/services/user.service.ts
+++ client/src/app/shared/services/user.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
@@ -12,6 +12,11 @@ export class UserService {
 
   constructor(private http:HttpClient) { }
 
+  login(user: User) : Observable<User>
+  {
+    return this.http.post<User>(environment.url+'user/Login',user);
+  }
+
   registerTeacher(teacher:User):Observable<number>
   {
     return this.http.post<number>(environment.url+'user/RegisterTeacher',teacher);
@@ -20,6 +25,13 @@ export class UserService {
   registerStudent(student:User):Observable<number>
   {
     return this.http.post<number>(environment.url+'user/RegisterStudent',student);
+  }
+
+  getStudentByClass(listClass: string[])
+  {
+    let params = new HttpParams();
+    params = params.append('listClass', listClass.join(', '));
+    return this.http.get(environment.url + "GetListOfUsersById",{ params: params });
   }
 
   uploadStudentsDetailes(file: File):Observable<boolean> {
